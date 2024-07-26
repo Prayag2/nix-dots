8e41cb8854295492fae24d385130b31d83c8fe07
Prayag2 <prayagjain2@gmail.com>
2024-07-26 22:50:13 +0530

    feat: vscode extensions

--- hosts/prayag-nix/home/configs/vscode/default.nix
+++ hosts/prayag-nix/home/configs/vscode/default.nix
@@ -3,10 +3,17 @@
     userSettings = {
       "editor.fontFamily" = "'JetBrains Mono', 'monospace', monospace";
       "editor.fontSize" = 18;
+      "vim.useSystemClipboard" = true;
     };
     extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
       equinusocio.vsc-material-theme-icons
+      ms-vsliveshare.vsliveshare
+      github.copilot
+      github.copilot-chat
+      ms-python.python
+      ms-python.debugpy
+      esbenp.prettier-vscode
     ];
   };
 }
